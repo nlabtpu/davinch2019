4a283a7657cba67c807e73427bdd0df699c90598
TomoyoHirai <tomoyotomo1201@gmail.com>
2019-07-19 15:46:20 +0900

    rr

--- routes/module/mod_socket.js
+++ routes/module/mod_socket.js
@@ -1,11 +1,5 @@
 var socket_io = require('socket.io');
-io.sockets.on('connection', function(socket) {
-//この中に書くよ
 
-var socket_id = socket.id;
-
-
-});
 
 function socket(srv) {
   var io = socket_io.listen(srv);
