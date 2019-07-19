4b9d81d2a12d5caa36cf22236a2c538eb7624b6d
TomoyoHirai <tomoyotomo1201@gmail.com>
2019-07-19 17:22:23 +0900

    eew

--- routes/module/mod_socket.js
+++ routes/module/mod_socket.js
@@ -1,6 +1,7 @@
 var socket_io = require('socket.io');
 
 
+
 function socket(srv) {
   var io = socket_io.listen(srv);
   io.sockets.on('connection', function (socket) {
@@ -9,6 +10,12 @@ function socket(srv) {
     const id = socket.id;
     socket.on('demo' + id, d => io.emit('receive' + id, d))
   });
+      socket.on('client_to_server_personal', function(data) {
+        var id = socket.id;
+        name = data.value;
+        var personalMessage = "あなたは、" + name + "さんとして入室しました。"
+        io.to(id).emit('server_to_client', {value : personalMessage})
+    });
 }
 
 module.exports = socket;
